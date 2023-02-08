953352a536951088f9244b6c355563fc97dc24e8
Krimo Temam <krimolinch@gmail.com>
2023-02-08 15:40:06 +0100

    mrd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,10 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
-      <Script
+      {/* <Script
         id="fb-sf-chat"
         src="https://service.force.com/embeddedservice/5.0/esw.min.js"
-      />
+      /> */}
       <Script
         id="sf-chat"
         src="https://valobat--chatbot.sandbox.my.salesforce.com/embeddedservice/5.0/esw.min.js"
