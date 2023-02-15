1d7804286ba35089c8fd3cd5aebfd1aa4631b9f0
Krimo Temam <krimolinch@gmail.com>
2023-02-15 19:08:53 +0100

    r

--- src/utils/sf-chat.ts
+++ src/utils/sf-chat.ts
@@ -147,6 +147,13 @@ export function initChat(
   embedded_svc.settings.entryFeature = "LiveAgent";
 
   // toutEvent();
+  embedded_svc.addEventHandler("onAvailability", function (data) {
+    console.log(data);
+    if (document.getElementById("SuppliedEmail") !== null) {
+      document.getElementById("SuppliedEmail").value = "bolt@gmail.com";
+    }
+    // console.log(document.getElementById("SuppliedEmail"));
+  });
 
   embedded_svc.init(
     "https://valobat--chatbot.sandbox.my.salesforce.com",
@@ -173,139 +180,6 @@ export const popChatBox = () => {
   embedded_svc.bootstrapEmbeddedService();
 };
 const toutEvent = () => {
-  embedded_svc.addEventHandler("onHelpButtonClick", function (data) {
-    console.log("onHelpButtonClick event was fired.");
-  });
-
-  embedded_svc.addEventHandler("onChatRequestSuccess", function (data) {
-    console.log(
-      "onChatRequestSuccess event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onChatEstablished", function (data) {
-    console.log(
-      "onChatEstablished event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onChasitorMessage", function (data) {
-    console.log(
-      "onChasitorMessage event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onAgentMessage", function (data) {
-    console.log(
-      "onAgentMessage event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onChatConferenceInitiated", function (data) {
-    console.log(
-      "onChatConferenceInitiated event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onAgentJoinedConference", function (data) {
-    console.log(
-      "onAgentJoinedConference event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onAgentLeftConference", function (data) {
-    console.log(
-      "onAgentLeftConference event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onChatConferenceEnded", function (data) {
-    console.log(
-      "onChatConferenceEnded event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onChatTransferSuccessful", function (data) {
-    console.log(
-      "onChatTransferSuccessful event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onChatEndedByChasitor", function (data) {
-    console.log(
-      "onChatEndedByChasitor event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onChatEndedByAgent", function (data) {
-    console.log(
-      "onChatEndedByAgent event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onQueueUpdate", function (data) {
-    console.log(
-      "onQueueUpdate event was fired. liveAgentSessionKey was " +
-        data.liveAgentSessionKey +
-        "and queuePosition was " +
-        data.queuePosition
-    );
-  });
-
-  embedded_svc.addEventHandler("onIdleTimeoutOccurred", function (data) {
-    console.log(
-      "onIdleTimeoutOccurred event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onConnectionError", function (data) {
-    console.log(
-      "onConnectionError event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onClickSubmitButton", function (data) {
-    console.log(
-      "onClickSubmitButton event was fired.  liveAgentSessionKey was " +
-        data.liveAgentSessionKey
-    );
-  });
-
-  embedded_svc.addEventHandler("onInviteAccepted", function () {
-    console.log("onInviteAccepted event was fired.");
-  });
-
-  embedded_svc.addEventHandler("onInviteRejected", function () {
-    console.log("onInviteRejected event was fired.");
-  });
-
-  embedded_svc.addEventHandler("onInvitationResourceLoaded", function () {
-    console.log("onInvitationResourceLoaded event was fired.");
-  });
-
-  embedded_svc.addEventHandler("onSettingsCallCompleted", function (data) {
-    console.log(
-      "onSettingsCallCompleted event was fired. Agent availability status is " +
-        data.isAgentAvailable
-        ? "online"
-        : "offline"
-    );
-    console.log(document.getElementById("SuppliedEmail"));
-  });
-
   embedded_svc.addEventHandler("onAvailability", function (data) {
     console.log(
       "onAvailability event was fired. Agent availability status is " +
